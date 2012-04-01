ed63cbecb86343cc498528284670f69fe4b4214a
Chris Anderson <chris@nullcode.org>
2012-04-01 15:07:00 -0700

    oxygen sort of working

--- molybear/entity.py
+++ molybear/entity.py
@@ -4,14 +4,18 @@ class Entity:
 	map_ref = None
 	x = 0
 	y = 0
+	move_wait = 0
 	alive = True
 	sprite = None
 	sprite_alive = None
 	sprite_dead = None
+	player = False
+	oxygen = 100
 	
-	def __init__(self, x, y, map_ref, alive_id, dead_id, sprite_cache):
+	def __init__(self, x, y, map_ref, alive_id, dead_id, sprite_cache, player = False):
 		self.map_ref = map_ref
 		self.x, self.y = x, y
+		self.player = player
 		
 		self.sprite_alive = sprite_cache.get_sprite(object_map[alive_id][0])
 		self.sprite_dead = sprite_cache.get_sprite(object_map[dead_id][0])
@@ -34,9 +38,10 @@ class Entity:
 	def move_to(self, x, y):
 		if self.valid_move(x, y):
 			entity = self.map_ref[x, y].entity
-			if entity:
+			if entity and self.player:
 				if entity.alive:
 					entity.alive = False
+					self.oxygen += 25
 
 			self.map_ref[self.x, self.y].entity = None
 			self.x, self.y = x, y
@@ -46,11 +51,12 @@ class Entity:
 		return False
 
 	def valid_move(self, x, y):
+		if not self.alive:
+			return False
 		if x < 0 or x >= self.map_ref.x:
 			return False
 		if y < 0 or y >= self.map_ref.y:
 			return False
-		print x, y, self.map_ref[x, y]
 		if not self.map_ref[x, y].valid_move:
 			return False
 
